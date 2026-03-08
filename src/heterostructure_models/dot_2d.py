5ddb4cdcd14848ee28ac3d40d5c12cea31333199
Merritt Losert <merritt.losert@gmail.com>
2026-03-08 18:40:44 -0400

    implement custom step size and locations

--- src/heterostructure_models/dot_2d.py
+++ src/heterostructure_models/dot_2d.py
@@ -27,14 +27,13 @@ class Dot2D:
 
     # For single-step model, the index of the step location along x, in units of dx.
     # For staircase models, the index is the location of one of the steps, and the rest are determined by step_spacing.
-    step_position_index: int | None = None 
+    # For custom step models, the list of step locations along x, in units of dx
+    step_position: int | None = None 
     step_spacing: int | None = None # for staircase model, the spacing between steps, in units of dx
 
-    # For staircase and custom step models, the list of step locations along x, in units of dx
-    step_location_list: list[int] | None = None
-
-    # For custom steps, we can choose to have upward or downward steps
-    step_directions: list[int] | int | None = None
+    # For custom steps, we can choose to have upward or downward steps.
+    # Can also be used to set the single-step or staircase step size.
+    step_magnitudes: list[int] | int | None = None
     
     def __post_init__(self):
         self.n_layers = len(self.si_concentrations)
@@ -52,36 +51,65 @@ class Dot2D:
             self._n_eff_y = 2 * np.sqrt(2 * np.pi) * ay * self.dx / constants.SI_LATTICE_CONSTANT**2
 
         if self.step_model == 'single-step' or self.step_model == 'staircase':
-            if self.step_position_index is None:
-                raise ValueError('Must specify step_position_index for single-step or staircase models.')
-
-            elif self.step_position_index < 0 or self.step_position_index >= self.nx:
-                raise ValueError('step_position_index must be between 0 and nx-1.')
+            if self.step_position is None or not isinstance(self.step_position, int):
+                raise ValueError('Must specify an integer step_position for single-step or staircase models.')
             
-        elif self.step_model == 'staircase':
-            if self.step_spacing is None:
-                raise ValueError('Must specify step_spacing for staircase model.')
+            elif self.step_position < 0 or self.step_position >= self.nx:
+                raise ValueError('step_position must be between 0 and nx-1.')
+            
+            if self.step_model == 'single-step':
+                if self.step_magnitudes is None:
+                    self._step_magnitudes = 1
+                elif isinstance(self.step_magnitudes, int):
+                    self._step_magnitudes = self.step_magnitudes
+                else:
+                    raise ValueError("step_magnitudes must be an integer or None for single-step")
+
+            if self.step_model == 'staircase':
+                if self.step_magnitudes is None:
+                    self._step_magnitudes = 1
+                elif isinstance(self.step_magnitudes, int):
+                    self._step_magnitudes = self.step_magnitudes
+                else:
+                    raise ValueError("step_magnitudes must be an integer or None for single-step")
+                
+                if self.step_spacing is None:
+                    raise ValueError('Must specify step_spacing for staircase model.')
+            
+                step_list = []
+
+                for j in range(self.nx):
+                    if (self.step_position-j) % self.step_spacing == 0:
+                        step_list.append(j)
+
+                self._step_location_list = step_list
+                self._step_magnitudes = self._step_magnitudes * np.ones_like(step_list)
+
                         
         elif self.step_model == 'custom':
-            if self.step_location_list is None:
-                raise ValueError('Must specify step_location_list for custom step model.')
+            if self.step_position is None or not (isinstance(self.step_position, int) or isinstance(self.step_position, list)):
+                raise ValueError('Must specify step_position (either int or list[int]) for custom step model.')
             
-            # Handle custom step directions
-            if self.step_directions is None:
-                self._step_directions = np.ones_like(self.step_location_list)
+            if isinstance(self.step_position, int):
+                self._step_location_list = [self.step_position]
+            else:
+                self._step_location_list = self.step_position
             
-            elif self.step_directions == 1 or self.step_directions == -1:
-                self._step_directions = self.step_directions * np.ones_like(self.step_location_list)
+            # Handle custom step magnitudes
+            if self.step_magnitudes is None:
+                self._step_magnitudes = np.ones_like(self.step_position)
             
-            elif len(self.step_directions) != len(self.step_location_list):
-                raise ValueError('step_direction must either be a number (0,1) or a list of equal length to step_location_list.')
+            elif isinstance(self.step_magnitudes, int):
+                self._step_magnitudes = self.step_magnitudes * np.ones_like(self.step_position)
+            
+            elif len(self.step_magnitudes) != len(self.step_position):
+                raise ValueError('step_magnitudes must either be an integer or a list of equal length to step_position.')
             
             else:
-                self._step_directions = np.sign(self.step_directions)
+                self._step_magnitudes = self.step_magnitudes
 
 
             
-
         if self.step_model == 'none':
             # Use the relevant function to generate the 2D lattice, depending on the type of disorder used
             self._generate_2d_lattice_no_step()
@@ -89,20 +117,7 @@ class Dot2D:
         elif self.step_model == 'single-step':
             self._generate_2d_lattice_single_step()
 
-        elif self.step_model == 'staircase':
-
-            step_list = []
-
-            for j in range(self.nx):
-                if (self.step_position_index-j) % self.step_spacing == 0:
-                    step_list.append(j)
-
-            self.step_location_list = step_list
-            self._step_directions = np.ones_like(step_list)
-
-            self._generate_2d_lattice_from_step_list()
-
-        elif self.step_model == 'custom':
+        elif self.step_model == 'staircase' or self.step_model == 'custom':
             self._generate_2d_lattice_from_step_list()
 
 
@@ -130,15 +145,28 @@ class Dot2D:
         Returns a 2D square lattice with a single step, of dimensions nx by len(si_concentrations)-1
         """
 
-        nz = self.n_layers - 1
+        nz = self.n_layers - abs(self._step_magnitudes)
         lattice = np.zeros((self.nx, nz))
+
+        if self._step_magnitudes > 0:
+            pos_step = True 
+        else:
+            pos_step = False
+
 
         for i in range(self.nx):
             for k in range(nz):
-                if i < self.step_position_index:
-                    xk = self.si_concentrations[k]
+                if i < self.step_position:
+                    if pos_step:
+                        xk = self.si_concentrations[k]
+                    else:
+                        xk = self.si_concentrations[k + abs(self._step_magnitudes)]
+
                 else:
-                    xk = self.si_concentrations[k+1]
+                    if pos_step:
+                        xk = self.si_concentrations[k + self._step_magnitudes]
+                    else:
+                        xk = self.si_concentrations[k]
 
                 if self.disorder_model == 'alloy':
                     #lattice[i,k] = np.random.binomial(self._n_eff_y, xk)/self._n_eff_y
@@ -153,17 +181,15 @@ class Dot2D:
 
     def _generate_2d_lattice_from_step_list(self):
         """
-        Returns a 2D square lattice with a staircase of steps, where the location of the steps is stored in self.step_location_list.
+        Returns a 2D square lattice with a staircase of steps, where the location of the steps is stored in self.step_position.
         """
 
-        total_num_steps = len(self.step_location_list)
+        total_step_offset = int(np.sum(self._step_magnitudes))
 
-        total_step_offset = int(np.sum(self._step_directions))
-
-        accumulated_offset = np.array([np.sum(self._step_directions[:l]) for l in range(len(self.step_location_list))]).astype(int)
+        accumulated_offset = np.array([np.sum(self._step_magnitudes[:l]) for l in range(len(self._step_location_list))]).astype(int)
         max_offset = accumulated_offset[np.argmax(np.abs(accumulated_offset))]
 
-        nz = len(self.si_concentrations) - (total_num_steps)
+        nz = len(self.si_concentrations) - (max_offset)
         lattice = np.zeros((self.nx, nz))
 
         for i in range(self.nx):
@@ -171,10 +197,10 @@ class Dot2D:
 
                 # Find which plateau a given x location should be on
                 plateau_found = False
-                for l in range(len(self.step_location_list)):
+                for l in range(len(self._step_location_list)):
                     if not plateau_found:
-                        step_offset = int(np.sum(self._step_directions[:l]))
-                        step_loc = self.step_location_list[l]
+                        step_offset = int(np.sum(self._step_magnitudes[:l]))
+                        step_loc = self._step_location_list[l]
 
                         # if i is less than a step location, then we know it belongs on the plateau
                         # that ends at that location
@@ -185,7 +211,7 @@ class Dot2D:
 
                 # if no plateau has yet been found, then the pleateau is the farthest right.
                 if not plateau_found:
-                    xk = self.si_concentrations[k+total_step_offset - max_offset]
+                    xk = self.si_concentrations[k + total_step_offset - max_offset]
 
                 if self.disorder_model == 'alloy':
                     #lattice[i,k] = np.random.binomial(self._n_eff_y, xk)/self._n_eff_y
